e737eb516cb52774b4dd3fcb918ee33953a0f70b
Vyacheslav Gurin <70888287+drimstor@users.noreply.github.com>
2022-08-31 18:42:21 +0300

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,17 +1,12 @@
 (function () {
   const btnOn = document.querySelector("#btnOn");
   const btnOff = document.querySelector("#btnOff");
-  window.location.href =
-    window.location.protocol +
-    "//" +
-    window.location.host +
-    window.location.pathname;
   btnOn.addEventListener("click", function () {
     localStorage.setItem("JS", "ON");
-    window.location.href;
+    window.location.reload();
   });
   btnOff.addEventListener("click", function () {
     localStorage.clear();
-    window.location.href;
+    window.location.reload();
   });
 })();
