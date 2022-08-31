ecfd12f3861edc4d04be18bbd91e8c34e53e0de2
Vyacheslav Gurin <70888287+drimstor@users.noreply.github.com>
2022-08-31 16:24:48 +0300

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -4,9 +4,14 @@
     if (btn.hasAttribute("data-boolean")) {
       btn.removeAttribute("data-boolean");
       console.log(btn);
+      localStorage.setItem("myCat", "Tom");
+      console.log(window.localStorage);
+      window.location.reload();
     } else {
       btn.setAttribute("data-boolean", true);
       console.log(btn);
+      console.log(localStorage.getItem("myCat"));
+      window.location.reload();
     }
   });
 })();
